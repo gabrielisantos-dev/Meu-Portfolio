0d347119016aae03b0dae2e218b182dbd4de6fe4
gabrielicristina <gabrielicristina189@gmail.com>
2024-03-18 02:09:37 -0300

    feat: adding animations and changing fonts

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ function typeWriter(text, i) {
     nameElement.innerHTML += text.charAt(i);
     i++;
     setTimeout(() => typeWriter(text, i), 100);
+  } else {
+    setTimeout(() => eraseText(text), 2000);
+  }
+}
+
+function eraseText(text) {
+  let length = text.length;
+  if (length > 0) {
+    nameElement.innerHTML = text.substring(0, length - 1);
+    setTimeout(() => eraseText(text.substring(0, length - 1)), 100);
+  } else {
+    document.querySelector('.main').style.display = 'block';
+    document.querySelector('.background-container').style.display = 'none';
+    document.body.style.backgroundColor = '#030518';
   }
 }
 
